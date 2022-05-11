10278da6d56882ffb81a2e488fce61fbd163715c
s19424 <s19424@pjwstk.edu.pl>
2022-05-11 14:24:28 +0200

    Added IActorService with method

---
+++ eTickets/eTickets/Data/Services/IActorsService.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using eTickets.Models;
+
+namespace eTickets.Data.Services
+{
+    public interface IActorsService
+    {
+        IEnumerable<Actor> GetAll();
+        Actor GetById(int id);
+        void Add(Actor actor);
+        Actor Update(int id, Actor newActor);
+        void Delete(int id);
+    }
+}
